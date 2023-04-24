a6212ccea473869dabf5a976b730a10a852c552d
Joeri Samson <joeri@xaop.com>
2023-04-24 14:56:53 +0200

    New attempt at updating to Savon 1.2
    There is no longer a separate Savon::Wasabi::Document, so we patch
    Wasabi::Document with an authenticate method, need to figure out if
    that's meaningful at some point

--- lib/activesp/wasabi_authentication.rb
+++ lib/activesp/wasabi_authentication.rb
@@ -1,18 +1,16 @@
-module Savon
-  module Wasabi
-    class Document
-      def authenticate(options)
-        @request ||= HTTPI::Request.new
-        case options[:method]
-        when :ntlm
-          @request.auth.ntlm(options[:usename], options[:password])
-        when :basic
-          @request.auth.basic(options[:usename], options[:password])
-        when :digest
-          @request.auth.digest(options[:usename], options[:password])
-        when :gss_negotiate
-          @request.auth.gssnegotiate(options[:usename], options[:password])
-        end
+module Wasabi
+  class Document
+    def authenticate(options)
+      @request ||= HTTPI::Request.new
+      case options[:method]
+      when :ntlm
+        @request.auth.ntlm(options[:usename], options[:password])
+      when :basic
+        @request.auth.basic(options[:usename], options[:password])
+      when :digest
+        @request.auth.digest(options[:usename], options[:password])
+      when :gss_negotiate
+        @request.auth.gssnegotiate(options[:usename], options[:password])
       end
     end
   end
